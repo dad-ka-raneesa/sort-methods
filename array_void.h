c2ce2cda77bb2333322feea8dbf74f2a43f7e54b
Deepika <bhuvanadeepika017@gmail.com>
2020-05-20 21:21:11 +0530

    added array_void.c and array_void.h

---
+++ array_void.h
@@ -0,0 +1,25 @@
+#ifndef __ARRAYS_VOID_H_
+#define __ARRAYS_VOID_H_
+typedef void *Object;
+
+typedef enum
+{
+  False,
+  True
+} Bool;
+
+typedef struct
+{
+  Object *array;
+  int length;
+} Array;
+
+typedef Array *Array_ptr;
+
+Object create_int(int value);
+Object create_char(char value);
+int find_length(char *value);
+char *create_string(char *value);
+Array_ptr create_array_void(int);
+
+#endif
--- sort_arrays_void.h
+++ sort_arrays_void.h
@@ -5,33 +5,14 @@
 #include <stdlib.h>
 #include <string.h>
 #include "sort_arrays.h"
-
-typedef void *Object;
-
-typedef enum
-{
-  False,
-  True
-} Bool;
-
-typedef struct
-{
-  Object *array;
-  int length;
-} Array;
-
-typedef Array *Array_ptr;
+#include "array_void.h"
 
 typedef Bool (*Predicate)(Object, Object);
 typedef void (*Displayer)(Object);
 
-Object create_int(int value);
-Object create_char(char value);
-int find_length(char *value);
-char *create_string(char *value);
-Array_ptr create_array_void(int);
 void selection_sort_array_void(Array_ptr void_array, Predicate predicate);
 void bubble_sort(Array_ptr void_array, Predicate predicate);
 void bubble_sort_array_void(Array_ptr void_array, Predicate predicate);
 void insertion_sort_array_void(Array_ptr void_array, Predicate predicate);
+
 #endif
